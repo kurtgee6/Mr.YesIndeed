d805f441e960a6f344fe972b884f1d502906fa29
kurtgee6 <kurtgee11@gmail.com>
2017-10-09 17:10:46 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from "./components/navbar/navbar"
+import BodyImage from "./components/bodyImage/bodyImage"
 
-const App = () => <Navbar />;
+const App = () => 
+  <div>
+    <Navbar />
+    <BodyImage />
+  </div>
+  ;
 
 export default App;
---
+++ client/src/components/bodyImage/bodyImage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./bodyImage.css";
+
+class bodyImage extends Component {
+
+   render () {
+        return (
+            <div>
+                <body>
+                    <img alt="Curtis Glenn" className= "mainImage" src="https://img00.deviantart.net/14c4/i/2013/298/5/f/random_guy_by_marcelmarkov-d6rnc6k.png" />
+                </body>
+            </div>
+        )
+   }
+};
+
+export default bodyImage;
