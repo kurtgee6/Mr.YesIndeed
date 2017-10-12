527cf8e9476a1dee0be966ee2f2c3b7bfa1871ed
kurtgee6 <kurtgee11@gmail.com>
2017-10-12 12:24:26 -0400

    added pictures

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const About = () =>
   <div>
-      <h1>This is the about page</h1>
+    <h1> This is the about page</h1>
+      <img src="https://s3-us-west-1.amazonaws.com/powr/defaults/image-slider2.jpg" />
   </div>;
 
 export default About;
